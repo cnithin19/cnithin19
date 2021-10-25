14f902a936ff5081173a401cbbf8fd73391b44d4
cnithin19 <71375232+cnithin19@users.noreply.github.com>
2021-10-25 23:06:46 +0530

    Add files via upload
    Add two numbers in java

---
+++ sample.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class sample{
+    public static void main(String... args) {
+        int i,j,k;
+    Scanner sc=new Scanner(System.in);
+    i=sc.nextInt();
+    j=sc.nextInt();
+    k=i+j;
+    System.out.println(k);
+    }
+    
+}
